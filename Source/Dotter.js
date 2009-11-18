ef79bdbfc28affa3f64a11f4c05be4ba834b0ed8
David Walsh <davidwalsh83@gmail.com>
2009-11-17 21:35:30 -0600

    Implementing document.id

--- Source/Dotter.js
+++ Source/Dotter.js
@@ -1,65 +1,63 @@
-(function($) { 
-	this.Dotter = new Class({
-	
-		/* implements */
-		Implements: [Options,Events],
+var Dotter = new Class({
 
-		/* options */
-		options: {
-			delay: 1000,
-			dot: '.',
-			message: 'Loading',
-			numDots: 3,
-			property: 'text',
-			reset: false,
-			onDot: function() {},
-			onStart: function() {},
-			onStop: function() {}
-		},
-	
-		/* initialization */
-		initialize: function(container,options) {
-			/* set options */
-			this.setOptions(options);
-			this.container = $(container);
-			this.dots = 0;
-			this.running = false;
-		},
-	
-		/* adds dot */
-		dot: function() {
-			if(this.running) {
-				var text = this.container.get(this.options.property);
-				this.dots++;
-				this.container.set(this.options.property,(this.dots % this.options.numDots != 0 ? text : this.options.message) + '' + this.options.dot);
-			}
-			return this;
-		},
-	
-		/* loads or resets the dotter */
-		load: function() {
-			this.loaded = true;
-			this.dots = 0;
-			this.dotter = function(){ this.dot(); this.fireEvent('dot'); }.bind(this);
-			this.periodical = this.dotter.periodical(this.options.delay);
-			this.container.set(this.options.property,this.options.message + '' + this.options.dot);
-			return this;
-		},
-	
-		/* start the dotter */
-		start: function() {
-			if(!this.loaded || this.options.reset) this.load();
-			this.running = true;
-			this.fireEvent('start');
-			return this;
-		},
-	
-		/* stop the dotter */
-		stop: function() {
-			this.running = this.loaded = false;
-			$clear(this.periodical);
-			this.fireEvent('stop');
-			return this;
+	/* implements */
+	Implements: [Options,Events],
+
+	/* options */
+	options: {
+		delay: 1000,
+		dot: '.',
+		message: 'Loading',
+		numDots: 3,
+		property: 'text',
+		reset: false,
+		onDot: function() {},
+		onStart: function() {},
+		onStop: function() {}
+	},
+
+	/* initialization */
+	initialize: function(container,options) {
+		/* set options */
+		this.setOptions(options);
+		this.container = document.id(container);
+		this.dots = 0;
+		this.running = false;
+	},
+
+	/* adds dot */
+	dot: function() {
+		if(this.running) {
+			var text = this.container.get(this.options.property);
+			this.dots++;
+			this.container.set(this.options.property,(this.dots % this.options.numDots != 0 ? text : this.options.message) + '' + this.options.dot);
 		}
-	});
-})(document.id);
+		return this;
+	},
+
+	/* loads or resets the dotter */
+	load: function() {
+		this.loaded = true;
+		this.dots = 0;
+		this.dotter = function(){ this.dot(); this.fireEvent('dot'); }.bind(this);
+		this.periodical = this.dotter.periodical(this.options.delay);
+		this.container.set(this.options.property,this.options.message + '' + this.options.dot);
+		return this;
+	},
+
+	/* start the dotter */
+	start: function() {
+		if(!this.loaded || this.options.reset) this.load();
+		this.running = true;
+		this.fireEvent('start');
+		return this;
+	},
+
+	/* stop the dotter */
+	stop: function() {
+		this.running = this.loaded = false;
+		$clear(this.periodical);
+		this.fireEvent('stop');
+		return this;
+	}
+});
